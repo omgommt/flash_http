631dce2921d59259db63a93234923a7d18c0a35e
rohit nahata <Rohit.nahata@makemytrip.com>
2020-04-01 17:55:55 +0530

    added support for redirect in DoFlashHttp

--- flash_test.go
+++ flash_test.go
@@ -8,7 +8,7 @@ import (
 func TestFlashHttp(t *testing.T) {
 	request := NewHTTPRequest()
 	request.URL = "https://www.google.com"
-	res, err := DoFlashHttp(request)
+	res, err := DoFlashHttp(request, 0)
 	if err != nil {
 		t.Errorf("Flashhttp error %v ", err)
 	} else if res == nil {
@@ -18,13 +18,14 @@ func TestFlashHttp(t *testing.T) {
 	} else if len(res.Body) == 0 {
 		t.Errorf("Flashhttp blank body")
 	}
+
 }
 
 func TestFlashHttpSkipLogs(t *testing.T) {
 	request := NewHTTPRequest()
 	request.URL = "https://www.google.com"
 	request.SkipLogs = true
-	res, err := DoFlashHttp(request)
+	res, err := DoFlashHttp(request, 0)
 	if err != nil {
 		t.Errorf("Flashhttp error %v ", err)
 	} else if res == nil {
@@ -39,10 +40,27 @@ func TestFlashHttpSkipLogs(t *testing.T) {
 func TestFlashHttpErrorHandled(t *testing.T) {
 	request := NewHTTPRequest()
 	request.URL = "https://www.google.unknown.com"
-	res, err := DoFlashHttp(request)
+	res, err := DoFlashHttp(request, 0)
 	if err == nil {
 		t.Errorf("Flashhttp no error ")
 	} else if res.HttpStatus == http.StatusOK {
 		t.Errorf("Flashhttp status = %v", res.HttpStatus)
 	}
 }
+
+func TestFlashHttpWithReDirect(t *testing.T) {
+	request := NewHTTPRequest()
+	request.URL = "https://www.google.com"
+	res, err := DoFlashHttp(request, 1)
+	if err != nil {
+		t.Errorf("Flashhttp error %v ", err)
+	} else if res == nil {
+		t.Errorf("Flashhttp blnk response")
+	} else if res.HttpStatus != http.StatusOK {
+		t.Errorf("Flashhttp status = %v", res.HttpStatus)
+	} else if len(res.Body) == 0 {
+		t.Errorf("Flashhttp blank body")
+	}
+
+}
+
